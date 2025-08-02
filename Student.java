518a1ba05f91e51b2a8c03d59f9a64fc0882d011
StellarX2 <skobelkin2017@yandex.ru>
2025-08-03 02:33:25 +0300

    Create Student.java

---
+++ Student.java
@@ -0,0 +1,20 @@
+import java.util.List;
+
+class Student {
+    private final String name;
+    private final List<Book> books;
+
+    public Student(String name, List<Book> books) {
+        this.name = name;
+        this.books = books;
+    }
+
+    public List<Book> getBooks() {
+        return books;
+    }
+
+    @Override
+    public String toString() {
+        return name + ": " + books;
+    }
+}
