ec9ad2ce2d23fc2c9609c85892dce92982ca9087
StellarX2 <skobelkin2017@yandex.ru>
2025-08-03 03:11:46 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,16 +1,19 @@
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
         List<Student> students = Arrays.asList(
             new Student("Алиса", Arrays.asList(
-                 new Book("Эффективная Java", 412, 2018),
                 new Book("Чистый код", 464, 2008),
+                new Book("Эффективная Java", 412, 2018),
                 new Book("Параллелизм в Java", 384, 2006),
                 new Book("Java 8 в действии", 424, 2014),
-                new Book("Java для начинающих", 720, 2005)
+                new Book("Java для начинающих", 720, 2005),
+                new Book("Сумерки", 1234, 1020)
             )),
             new Student("Боб", Arrays.asList(
+                new Book("Чистый код", 464, 2008),
                 new Book("Основы Java", 928, 2016),
                 new Book("Головоломки Java", 312, 2005),
                 new Book("Философия Java", 1150, 2006),
@@ -19,19 +22,31 @@ public class Main {
             ))
         );
 
-        students.stream()
-            .peek(System.out::println)
-            .map(Student::getBooks)
-            .flatMap(Collection::stream)
-            .sorted()
-            .distinct()
-            .filter(b -> b.getYear() > 2000)
-            .limit(3)
-            .map(Book::getYear)
-            .findFirst()
-            .ifPresentOrElse(
-                year -> System.out.println("Год выпуска книги: " + year),
-                () -> System.out.println("Книги не найдены")
-            );
+        // для каждого студента:
+        students.forEach(student -> {
+            System.out.println("Студент" + student.getName());
+
+            // обработка книг через стрим
+            List<Book> filteredBooks = student.getBooks().stream()
+                .sorted(Comparator.comparingInt(Book::getPages)) // сортировка по страницам
+                .distinct() // уникальные (по equals)
+                .filter(book -> book.getYear() > 2000) // только после 2000
+                .limit(3) // максимум 3 книги
+                .collect(Collectors.toList());
+
+            // вывод книг
+            filteredBooks.forEach(book -> System.out.println(" Книга " + book));
+
+            // short-circuit + Optional
+            filteredBooks.stream()
+                .map(Book::getYear)
+                .findFirst()
+                .ifPresentOrElse(
+                    year -> System.out.println("Год первой книги: " + year),
+                    () -> System.out.println("Книг после 2000 года нет")
+                );
+
+            System.out.println(); // пустая строка между студентами
+        });
     }
 }
